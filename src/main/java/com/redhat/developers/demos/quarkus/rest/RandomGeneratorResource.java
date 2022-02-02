2abf0c3a5231d426dac92d05bcaf77e0b5754c7f
Rohan Kumar <rohaan@redhat.com>
2022-02-02 20:10:53 +0530

    feat: Initial Commit
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ src/main/java/com/redhat/developers/demos/quarkus/rest/RandomGeneratorResource.java
@@ -0,0 +1,19 @@
+package com.redhat.developers.demos.quarkus.rest;
+
+import com.redhat.developers.demos.quarkus.service.RandomGeneratorService;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/random")
+public class RandomGeneratorResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        RandomGeneratorService randomGeneratorService = new RandomGeneratorService();
+        return Response.ok(randomGeneratorService.createRandomMessage()).build();
+    }
+}
